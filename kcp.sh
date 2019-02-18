9f677c1861f592a64399611e3044ca9267083b12
笑饮砒霜 <qpzm1258@163.com>
2019-02-18 20:29:52 +0800

    user quiet

--- kcp.sh
+++ kcp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kcpclient -r $SERVER_IP:$KCP_SERVER_PORT -l :8964 -mode fast2 -mtu 1400 -key 131415 -crypt none -datashard 0 -parityshard 0 -sndwnd 512 -rcvwnd 2048 -nocomp true -quiet &
+kcpclient -r $SERVER_IP:$KCP_SERVER_PORT -l :8964 -mode fast2 -mtu 1400 -key 131415 -crypt none -datashard 0 -parityshard 0 -sndwnd 512 -rcvwnd 2048 -nocomp true -quiet true -conn 2 &
