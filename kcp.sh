be16c91ee82ec2d01742bd621a478046b2644e3b
笑饮砒霜 <qpzm1258@163.com>
2019-10-23 14:29:54 +0800

    use '--tcp'

--- kcp.sh
+++ kcp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kcpclient -r $SERVER_IP:$KCP_SERVER_PORT -l :8964 -mode fast2 -mtu 1400 -key 131415 -crypt none -datashard 0 -parityshard 0 -sndwnd 512 -rcvwnd 2048 --nocomp --quiet --conn 1 &
+kcpclient -r $SERVER_IP:$KCP_SERVER_PORT -l :8964 --mode fast2 --mtu 1400 --key 131415 --crypt none --datashard 0 --parityshard 0 --sndwnd 512 --rcvwnd 2048 --nocomp --quiet --conn 2 --tcp &
